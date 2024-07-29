aa47ed84b2283068ad2ec06ecdc88ad8073dcb6a
Dennis moseti <dudubot404@gmail.com>
2024-07-29 14:06:53 +0300

    ui design: index template updated with helper model functions & styles view

--- model/release.go
+++ model/release.go
@@ -1,8 +1,22 @@
 package model
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
 type Release struct {
 	Title, Category, Synopsis, Thumbnail, Trailer, Url, Id string
 	Date                                                   time.Time
+}
+
+func (r Release) HumanDate() string {
+	yrs := r.Date.Year()
+	dy := r.Date.Day()
+	mnt := r.Date.Month().String()[:3]
+	return strconv.Itoa(dy) + " " + mnt + " " + strconv.Itoa(yrs)
+}
+
+func (r Release) Future() bool {
+	return r.Date.After(time.Now())
 }
