d56456876e678fa0fecb5e054d90a677be45a5c7
Dennis moseti <dudubot404@gmail.com>
2024-08-06 16:25:04 +0300

    Model struct: timeline model; timeline structure model rep update with new functions & fields

--- model/timeline.go
+++ model/timeline.go
@@ -1,8 +1,32 @@
 package model
 
-import "time"
+import (
+	"math"
+	"strconv"
+	"time"
+)
 
 type Timeline struct {
 	Title, Category, Id, Ref string
 	Date                     time.Time
+	Index                    int
+	Left                     bool
+}
+
+func (t Timeline) Month() string {
+	return t.Date.Month().String()[:3]
+}
+
+func (t Timeline) Day() string {
+	d := strconv.Itoa(t.Date.Day())
+	if len(d) < 2 {
+		return "0" + d
+	} else {
+		return strconv.Itoa(t.Date.Day())
+	}
+}
+
+func (t Timeline) Pos() string {
+	dy := float32(t.Date.YearDay()) / float32(366+31) * 100
+	return strconv.Itoa(int(math.Ceil(float64(dy))))
 }
