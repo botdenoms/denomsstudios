845563a5bbda317234644791753eb7ab822c8009
Dennis moseti <dudubot404@gmail.com>
2024-08-30 03:40:23 +0300

    ui update: dashboard page; ui design implemented with css styling &  limited js functionality

---
+++ public/admin.js
@@ -0,0 +1,12 @@
+// 
+function switchTab(index) {
+    var tabsP = document.getElementById("tabs")
+    if (tabsP.children.item(index).className.includes("active")) {
+        return
+    }
+    for (const child of tabsP.children) {
+        child.className = 'tb'
+    }
+    tabsP.children.item(index).className += " active"
+}
+
