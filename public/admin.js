d1f7f7cd5784f26ae65a0658a4b5bf1bcc07efd4
Dennis moseti <dudubot404@gmail.com>
2024-09-03 10:28:59 +0300

    ui update: tabs switching ; ui design update with js functionality

--- public/admin.js
+++ public/admin.js
@@ -8,6 +8,40 @@ function switchTab(index) {
         child.className = 'tb'
     }
     tabsP.children.item(index).className += " active"
+
+    var dshtb = document.getElementById("dshb")
+    var tltb = document.getElementById("tln")
+    var rltb = document.getElementById("rls")
+    var sttb = document.getElementById("stt")
+
+    switch (index) {
+        case 0:
+            dshtb.className = "dshbrd"
+            tltb.className = " hd"
+            rltb.className = " hd"
+            sttb.className = " hd"
+            break;
+        case 1:
+            tltb.className = "tlbrd"
+            dshtb.className = " hd"
+            rltb.className = " hd"
+            sttb.className = " hd"
+            break;
+        case 2:
+            rltb.className ="rlsbrd"
+            tltb.className = " hd"
+            dshtb.className = " hd"
+            sttb.className = " hd"
+            break;
+        case 3:
+            sttb.className = "sttbrd"
+            tltb.className = " hd"
+            rltb.className = " hd"
+            dshtb.className = " hd"
+            break;
+        default:
+            break;
+    }
 }
 
 function timelineForm(){
