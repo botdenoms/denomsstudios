4e5cd0c21219f1b67ce6c2d07425a697cdfcdf54
Dennis moseti <dudubot404@gmail.com>
2024-09-08 18:55:56 +0300

    ui update:  dashboard -> Timeline tab ; ui interactions  actions update

--- public/admin.js
+++ public/admin.js
@@ -75,6 +75,12 @@ function propToggle(id) {
     }
     var props = document.getElementById(idv)
     props.className = "props hd"
+    var ntl = document.getElementById("ntitle")
+    var nct = document.getElementById("ncategory")
+    var ncl = document.getElementById("ndate")
+    ntl.className = "nttl hd"
+    nct.className = "ncat hd"
+    ncl.className = "ndt hd"
 }
 
 function timelineSelect(id, ref=false){
@@ -83,12 +89,28 @@ function timelineSelect(id, ref=false){
         clnm = "props1"
     }else{
         clnm = "props"
+        var ntl = document.getElementById("ntitle")
+        var nct = document.getElementById("ncategory")
+        var ncl = document.getElementById("ndate")
+        
+        ntl.className = "hd nttl"
+        nct.className = "hd ncat"
+        ncl.className = "hd ndt"
+
+        var props = document.getElementById(clnm)
+        var rw1 = props.getElementsByClassName("row1")
+        rw1.item(0).className = "row1 hd"
+
+
+        peekHd()
     }
     var props = document.getElementById(clnm)
+
     var itms = document.getElementsByClassName("itemtl")
     var title = ""
     var category = ""
     var date = null
+    var refId = null
 
     for (let index = 0; index < itms.length; index++) {
         if(itms.item(index).getAttribute("data-id") === id){
@@ -96,22 +118,135 @@ function timelineSelect(id, ref=false){
             title = itms.item(index).getAttribute("data-title")
             category = itms.item(index).getAttribute("data-category")
             date = itms.item(index).getAttribute("data-date")
+            refId = itms.item(index).getAttribute("data-ref")
         }else{
             itms.item(index).className = "itemtl"
         }
-
     }
 
     var tl = props.getElementsByClassName("tlwrd")
     var ct = props.getElementsByClassName("ctgwht")
     var cl = props.getElementsByClassName("col1")
+
     tl.item(0).innerHTML = title
     ct.item(0).innerHTML = category
     cl.item(0).children.item(1).innerHTML = date
+
+    tl.item(0).className = "tlwrd"
+    ct.item(0).className = "ctgwht"
+    cl.item(0).className = "col1"
+    if (ref) {
+        // 
+    }else{
+        if (refId === "") {
+            var edit = props.getElementsByClassName("edit")
+            var del = props.getElementsByClassName("del")
+            var peek = props.getElementsByClassName("pkb")
+            var rls = props.getElementsByClassName("rels")
+            edit.item(0).className = "btnb edit"
+            del.item(0).className = "btnr del"
+            peek.item(0).className = "btnb pkb hd"
+            rls.item(0).className = "btnb rels hd"
+        }else{
+            var edit = props.getElementsByClassName("edit")
+            var del = props.getElementsByClassName("del")
+            var peek = props.getElementsByClassName("pkb")
+            var rls = props.getElementsByClassName("rels")
+            edit.item(0).className = "btnb edit hd"
+            del.item(0).className = "btnr del hd"
+            peek.item(0).className = "btnb pkb"
+            rls.item(0).className = "btnb rels"
+            rls.item(0).setAttribute("href", `/release?id=${refId}`)
+        }
+    }
     props.className = "props"
 }
 
 function nextRelease(index, id) {
     switchTab(index)
     timelineSelect(id, true)
+}
+
+function peekShow() {
+    var btn = document.getElementById("pkb")
+    var vw = document.getElementById("peek")
+    btn.setAttribute("disabled", true)
+    vw.className = "peek"
+}
+
+function peekHd() {
+    var vw = document.getElementById("peek")
+    var btn = document.getElementById("pkb")
+    vw.className = "peek hd"
+    btn.removeAttribute("disabled")
+}
+
+function editTimeline() {
+    var props = document.getElementById("props")
+    var tl = props.getElementsByClassName("tlwrd")
+    var ct = props.getElementsByClassName("ctgwht")
+    var cl = props.getElementsByClassName("col1")
+    var rw1 = props.getElementsByClassName("row1")
+    var ed = props.getElementsByClassName("edit")
+    var del = props.getElementsByClassName("del")
+
+    var ntl = document.getElementById("ntitle")
+    var nct = document.getElementById("ncategory")
+    var ncl = document.getElementById("ndate")
+
+    var ttl = tl.item(0).innerHTML 
+    var ctg = ct.item(0).innerHTML
+    var dt = cl.item(0).children.item(1).innerHTML
+
+    var ts = Date.parse(dt)
+    var rld = new Date(ts)
+    // hide the items
+    tl.item(0).className += " hd"
+    ct.item(0).className += " hd"
+    cl.item(0).className += " hd"
+
+    ed.item(0).className = "btnb edit hd"
+    del.item(0).className = "btnr del hd"
+    // show inputs
+    ntl.value = ttl
+    console.log(`${rld.getFullYear()}-${(rld.getMonth() + 1).toString().padStart(2, "0")}-${rld.getDate().toString().padStart(2, "0")}`)
+    ncl.value = `${rld.getFullYear()}-${(rld.getMonth() + 1).toString().padStart(2, "0")}-${rld.getDate().toString().padStart(2, "0")}`
+    ntl.className = "nttl"
+    nct.className = "ncat"
+    ncl.className = "ndt"
+
+    rw1.item(0).className = "row1"
+}
+
+function closeEdit() {
+    var ntl = document.getElementById("ntitle")
+    var nct = document.getElementById("ncategory")
+    var ncl = document.getElementById("ndate")
+    var props = document.getElementById("props")
+    var ed = props.getElementsByClassName("edit")
+    var del = props.getElementsByClassName("del")
+    var rw1 = props.getElementsByClassName("row1")
+    var tl = props.getElementsByClassName("tlwrd")
+    var ct = props.getElementsByClassName("ctgwht")
+    var cl = props.getElementsByClassName("col1")
+
+
+    tl.item(0).className = "tlwrd"
+    ct.item(0).className = "ctgwht"
+    cl.item(0).className = "col1"
+    
+    ntl.className = "hd nttl"
+    nct.className = "hd ncat"
+    ncl.className = "hd ndt"
+    rw1.item(0).className = "row1 hd"
+    ed.item(0).className = "btnb edit"
+    del.item(0).className = "btnr del"
+}
+
+function delTimeline() {
+    alert("Under development")
+}
+
+function fetchRelease(id) {
+    // get release of the given id 
 }
