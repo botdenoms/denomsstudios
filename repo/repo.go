8e17461648538037a72821f64791c461737d2db0
Dennis moseti <dudubot404@gmail.com>
2024-09-11 18:57:42 +0300

    Data repo: repo interfaces updated with more functionalities

--- repo/repo.go
+++ repo/repo.go
@@ -7,13 +7,19 @@ type Repository interface {
 
 	RandomRelease() model.Release
 
-	CreateRelease(items model.Release) (string, bool)
+	CreateRelease(rel model.Release) (string, bool)
+
+	CreateTimeline(tl model.Timeline) (string, bool)
 
 	AllRelease() []model.Release
 
 	AllTimeline() []model.Timeline
 
 	ReleaseById(id string) (model.Release, bool)
+
+	TimelineById(id string) (model.Timeline, bool)
+
+	UpdateTimeline(id string, data map[string]interface{}) (string, bool)
 
 	AllReleaseSorted() []model.Release
 
